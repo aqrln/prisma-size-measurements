d31c80eeee8cf66427e7cd034f041e98c669138b
Tom Houlé <tom@tomhoule.com>
2023-02-02 14:11:50 +0100

    Add clients, schemas

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
             echo "#!/bin/bash" > $OUTDIR/script.sh
             cat ${scriptFile} >> $OUTDIR/script.sh
             chmod +x $OUTDIR/script.sh
-            docker run --rm -it -v $OUTDIR:/root --entrypoint=bash docker.io/node:lts -c 'cd /root; echo "running the script"; ./script.sh'
+            docker run --rm -it -v /nix:/nix -v $OUTDIR:/root --entrypoint=bash docker.io/node:lts -c 'cd /root; echo "running the script"; ./script.sh'
 
 
             echo "=============================="
@@ -24,11 +24,56 @@
           '');
       in
       {
-        packages.vanillaSize = runInContainer "vanillaSize" ''
-          npm i prisma
-          echo hi > out.txt
-        '';
+        packages = {
+          vanillaSize = runInContainer "vanillaSize" ''
+            npm i prisma
+          '';
 
-        packages = { inherit (pkgs) diskonaut; };
+          smallVanillaPrismaClient = runInContainer "vanillaSize" ''
+            npm i prisma
+            cp ${./small_schema.prisma} schema.prisma
+            cat << EOF >> schema.prisma
+              generator jsclient {
+                provider = "prisma-client-js"
+              }
+            EOF
+            npx prisma generate
+          '';
+
+          largeVanillaPrismaClient = runInContainer "vanillaSize" ''
+            npm i prisma
+            cp ${./large_schema.prisma} schema.prisma
+            cat << EOF >> schema.prisma
+              generator jsclient {
+                provider = "prisma-client-js"
+              }
+            EOF
+            npx prisma generate
+          '';
+
+          smallEdgePrismaClient = runInContainer "vanillaSize" ''
+            npm i prisma
+            cp ${./small_schema.prisma} schema.prisma
+            cat << EOF >> schema.prisma
+              generator jsclient {
+                provider = "prisma-client-js"
+              }
+            EOF
+            npx prisma generate --data-proxy
+          '';
+
+          largeEdgePrismaClient = runInContainer "vanillaSize" ''
+            npm i prisma
+            cp ${./large_schema.prisma} schema.prisma
+            cat << EOF >> schema.prisma
+              generator jsclient {
+                provider = "prisma-client-js"
+              }
+            EOF
+            npx prisma generate --data-proxy
+          '';
+
+          inherit (pkgs) diskonaut;
+        };
       });
 }
