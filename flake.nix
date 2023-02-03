af0eba7313b405fb5637c73a1b5dca176ff9f9ab
Tom Houlé <tom@tomhoule.com>
2023-02-03 15:37:06 +0100

    Lambda cold start measurements setup and results

--- flake.nix
+++ flake.nix
@@ -76,12 +76,13 @@
 
 
           # last step:
-          # aws lambda update-function-code --function-name cold-start-test --zip-file "fileb://lambda-app.zip"
+          # aws lambda update-function-code --function-name cold-start-test --zip-file "fileb://lambda-app/lambda-app.zip"
           awsZip = pkgs.writeShellScriptBin "awsZip" ''
+            rm -rf lambda-app/node_modules
             docker run --rm -it -v /nix:/nix -v ./lambda-app:/home --entrypoint=bash docker.io/node:lts -c 'cd /home; npm i; npx prisma generate'
             pushd lambda-app
             : Delete the local binaries
-            find node_modules -name 'libquery*debian*' -delete
+            find node_modules -name '*debian-openssl*' -delete
             : Delete the CLI
             rm -rf node_modules/prisma
             rm lambda-app.zip
